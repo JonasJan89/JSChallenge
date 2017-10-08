7a0c8df441cde57f1bdb81b0d290273c910ae18d
Janczyk, Jonas <s58968@beuth-hochschule.de>
2017-10-08 20:35:52 +0200

    [#5] Add files

---
+++ react-backend/files/studentsCode/123_59d958281a033c9545370bb1.js
@@ -0,0 +1,10 @@
+const add = (a,b) => a+b;
+
+const sub = (a,b) => a-b;
+
+const mult = (a,b) => a*b;
+
+const divideValues = (a,b) => {
+    return b <= 0 ? -1 : a/b;
+};
+
---
+++ react-backend/files/tasks/Task01.js
@@ -0,0 +1,12 @@
+const add = (a,b) => a+b;
+
+const sub = (a,b) => a-b;
+
+const mult = (a,b) => a*a;
+
+const divideValues = (a,b) => {
+    if(b >= 0) {
+        return -1;
+    }
+    return a/b;
+};
